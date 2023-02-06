84e0bb0c7e04e847895e37fb1304a572ea52fc71
Ralph Lee <ralphlee8649@gmail.com>
2023-02-06 21:11:22 +0900

    #3

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,55 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.solution("2019.12.31", new String[]{"2019.12.31 15", "2020.01.01 14", "2020.01.02 14"},
-                new String[]{"2019.12.31 15", "2020.01.01 14", "2020.01.02 14"});
+        Solution slt = new Solution();
+
+        //날짜, 약관 배열, 사용자의 약관 동의 히스토리 배열
+        String today = "2022.05.19";
+        String[] terms = {"A\t6", "B\t12", "C\t3"};
+        String[] privacies = {"2021.05.02\tA", "2021.07.01\tB", "2022.02.19\tC", "2022.02.20\tC"};
+
+        //결과
+        System.out.println("result : " + Arrays.toString(slt.solution(today, terms, privacies)));
     }
 }
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
+        //오늘 날짜 환산
+        int todayDate = dateCalculator(today);
+
+        //전체 약관 map 만들기
         Map<String, Integer> termMap = new HashMap<>();
-        int date = getDate(today);
-
-        for (String s : terms) {
-            String[] term = s.split(" ");
-
+        for(int i = 0; i < terms.length; i++) {
+            String[] term = terms[i].trim().split("\t");
             termMap.put(term[0], Integer.parseInt(term[1]));
         }
-        for (int i = 0; i < privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
 
-            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
-                answer.add(i + 1);
+        //파기 대상 약관 리스트
+        List<Integer> expList = new ArrayList<>();
+        for(int i = 0; i < privacies.length; i++) {
+            String[] privacy = privacies[i].split("\t");
+            if(termMap.get(privacy[1]) * 28 <= todayDate - dateCalculator(privacy[0])) {
+                expList.add(i + 1);
             }
         }
-        return answer.stream().mapToInt(integer -> integer).toArray();
+
+        //결과 배열 생성 후 반환
+        int[] resultArr = new int[expList.size()];
+        for(int i = 0; i < expList.size(); i++) {
+            resultArr[i] = expList.get(i);
+        }
+        return resultArr;
     }
 
-    private int getDate(String today) {
+    //날짜 환산기(전체 날짜를 일 수로 환산)
+    private int dateCalculator(String today) {
         String[] date = today.split("\\.");
         int year = Integer.parseInt(date[0]);
         int month = Integer.parseInt(date[1]);
         int day = Integer.parseInt(date[2]);
-        int dayDate = (year * 12 * 28) + (month * 28) + day;
-        return dayDate;
+
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
